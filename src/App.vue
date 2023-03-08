8c4e43ba5fc679474b71269d4c150fb607ee13ae
Adeniyi Aderounmu <33498241+teebarg@users.noreply.github.com>
2023-03-08 07:28:23 +0100

    update (#26)
    * update
    
    * check
    
    * update
    
    * update
    
    * check
    
    * test
    
    * update
    
    * update
    
    * check
    
    * update
    
    * update
    
    * to-json
    
    * Hey
    
    * ci skip
    
    * ok
    
    * update
    
    * hmmmm
    
    * Ok now
    
    * Good
    
    * yes
    
    * Hope
    
    * update
    
    ---------
    
    Co-authored-by: Adeniyi <teebarg01@gmail.com>
    Co-authored-by: AltaraNg <34765609+AltaraNg@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <script setup>
 import { RouterView } from "vue-router";
-// import NavBar from "./components/Nav.vue";
-// import SideBarMain from './components/SideBarMain.vue'
-import Notification from "@/components/Notification.vue"
+import Notification from "@/components/Notification.vue";
 </script>
 
 <template>
-
   <!-- <div class="">
     <NavBar v-if="!$route.meta.noAuth" class="sticky top-0 z-50" />
     <div class="overflow-hidden lg:flex  block">
@@ -18,16 +15,14 @@ import Notification from "@/components/Notification.vue"
       <div class="flex items-center w-full   justify-center">
         <RouterView
         :class="!$route.meta.noAuth ? 'lg:w-full overflow-hiden' : 'w-full'"
-      />
-      </div>
-      
-    </div>
-  </div> -->
- <div class="flex items-center w-full   justify-center">
-        <RouterView
-        :class="!$route.meta.noAuth ? 'w-full overflow-hiden' : 'w-full'"
-      />
-      </div>
+          />
+          </div>
+
+        </div>
+      </div> -->
+  <div class="flex items-center w-full justify-center">
+    <RouterView :class="!$route.meta.noAuth ? 'w-full overflow-hiden' : 'w-full'" />
+  </div>
   <Notification />
 </template>
 <style>
