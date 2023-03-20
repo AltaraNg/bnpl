b635a18ca6ffdc22aaf41fa11dce914efcbe6abd
Blessing Onwunali <cheedinmarblessing@gmail.com>
2023-03-20 18:13:37 +0100

    fix table UI

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -4,7 +4,7 @@
             <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                 <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
                     <table class="min-w-full divide-y divide-gray-300">
-                        <thead class="bg-primary text-white">
+                        <thead class="bg-gray-50">
                             <tr>
                                 <slot name="columns"></slot>
                             </tr>
