bacb6ccf0314423f943d9e23068b885f42cccccf
Omodamola <darmhoo@hotmail.com>
2023-03-24 07:50:41 +0100

    update profile vue

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -43,18 +43,18 @@
                                     <div class="mt-6 flex flex-col lg:flex-row">
                                         <div class="flex-grow space-y-6">
                                             <div>
-                                                <label for="username" class="block text-sm font-medium text-gray-700">Full Name</label>
+                                                <label for="full_name" class="block text-sm font-medium text-gray-700">Full Name</label>
                                                 <div class="mt-1 flex rounded-md shadow-sm">                                                    
-                                                    <input type="text" name="username" id="username" autocomplete="username" disabled
+                                                    <input type="text" name="full_name" id="full_name" autocomplete="full_name" disabled
                                                         class="block w-full min-w-0 flex-grow rounded-none rounded-r-md border-gray-300 focus:border-sky-500 focus:ring-sky-500 sm:text-sm"
                                                         :value="userdata.full_name" />
                                                 </div>
                                             </div>
 
                                             <div>
-                                                <label for="about" class="block text-sm font-medium text-gray-700">Email</label>
+                                                <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
                                                 <div class="mt-1">
-                                                    <input type="text" name="username" id="username" autocomplete="username" disabled
+                                                    <input type="text" name="email" id="email" autocomplete="email" disabled
                                                         class="block w-full min-w-0 flex-grow rounded-none rounded-r-md border-gray-300 focus:border-sky-500 focus:ring-sky-500 sm:text-sm"
                                                         :value="userdata.email" />
                                                 </div>
@@ -98,15 +98,15 @@
 
                                     <div class="mt-6 grid grid-cols-12 gap-6">
                                         <div class="col-span-12 sm:col-span-6">
-                                            <label for="first-name" class="block text-sm font-medium text-gray-700">Phone Number</label>
-                                            <input type="text" name="first-name" id="first-name" autocomplete="given-name" disabled v-model="userdata.phone_number"
+                                            <label for="phone_number" class="block text-sm font-medium text-gray-700">Phone Number</label>
+                                            <input type="text" name="phone-number" id="phone-number" autocomplete="phone-number" disabled v-model="userdata.phone_number"
                                                 class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm" />
                                         </div>
 
                                         <div class="col-span-12 sm:col-span-6">
-                                            <label for="last-name" class="block text-sm font-medium text-gray-700">Vendor ID</label>
-                                            <input type="text" name="last-name" id="last-name" autocomplete="family-name" v-model="userdata.staff_id"
-                                                class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm" />
+                                            <label for="vendor-id" class="block text-sm font-medium text-gray-700">Vendor ID</label>
+                                            <input type="text" name="vendor-id" id="vendor-id" autocomplete="vendor-ide" v-model="userdata.staff_id"
+                                                class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm" disabled />
                                         </div>
 
                                         <div class="col-span-12">
@@ -116,9 +116,9 @@
                                         </div>
 
                                         <div class="col-span-12 sm:col-span-6">
-                                            <label for="company" class="block text-sm font-medium text-gray-700">Address</label>
-                                            <input type="text" name="company" id="company" autocomplete="organization" v-model="userdata.address"
-                                                class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm" />
+                                            <label for="address" class="block text-sm font-medium text-gray-700">Address</label>
+                                            <input type="text" name="address" id="address" autocomplete="address" v-model="userdata.address"
+                                                class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm" disabled/>
                                         </div>
                                     </div>
                                 </div>
