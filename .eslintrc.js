bb1079a3b33a7bdb104fd8f0f2033f1b31737127
Adeniyi <teebarg01@gmail.com>
2023-09-06 11:31:51 +0100

    linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,23 @@
 module.exports = {
-    root: true,
-    env: {
-        node: true,
-    },
-    extends: ["plugin:vue/vue3-essential", "eslint:recommended"],
-    parserOptions: {
-        parser: "@babel/eslint-parser",
-    },
-    rules: {
-        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-        "vue/multi-word-component-names": "warn",
-        "vue/no-mutating-props": "error",
-    },
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "plugin:prettier/recommended",
+    "eslint:recommended",
+  ],
+  plugins: ["prettier"],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": "error",
+    "vue/multi-word-component-names": "warn",
+    "vue/no-mutating-props": "error",
+  },
 };
