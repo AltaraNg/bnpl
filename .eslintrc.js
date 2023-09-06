693687a7dceafc2a916463bacb9ae1106cbc1ae8
Adeniyi <teebarg01@gmail.com>
2023-09-06 13:12:46 +0100

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,18 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-    "prettier",
-  ],
-  plugins: ["prettier"],
-  parser: "vue-eslint-parser",
-  parserOptions: {
-    parser: "@babel/eslint-parser",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "prettier/prettier": "error",
-    "vue/multi-word-component-names": "warn",
-    "vue/no-mutating-props": "error",
-  },
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: ["plugin:vue/vue3-essential", "eslint:recommended", "prettier"],
+    plugins: ["prettier"],
+    parser: "vue-eslint-parser",
+    parserOptions: {
+        parser: "@babel/eslint-parser",
+    },
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "vue/multi-word-component-names": "warn",
+        "vue/no-mutating-props": "error",
+    },
 };
